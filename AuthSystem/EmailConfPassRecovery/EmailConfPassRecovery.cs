304ff5f8bcca2a11ee275d4a458e2db08251d8e3
rymanbod <rymanbod@RYM1080>
2021-09-17 10:19:41 +0200

    Add project files.

---
+++ AuthSystem/EmailConfPassRecovery/EmailConfPassRecovery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*Account confirmation and password recovery in ASP.NET Core på Microsoft.docs för att lägga till email-bekräftelse och för att återhämta lösenord om man glömt*/
+namespace AuthSystem.EmailConfPassRecovery
+{
+    public class EmailConfPassRecovery
+    {
+    }
+}
